6d0e1d7c10aeee256bc9ff552fcb7b166c3842fe
root <root@HW0418.qentelli.io>
2021-08-19 13:13:20 +0530

    first commit for pollscm

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#starting shell scripting
+
+a='my name is Karthik'
+
+echo $a
+#	if { $a -eq file }
+#	then echo a is a file
+#	else
+#	echo a is not a file
+#	fi
+date
+
+echo "Hello World this is my first script"
